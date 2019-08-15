633b6301770dd64dc4c2b6d45e6b1965766b5bfd
Jo Jinsik <kunggom@gmail.com>
2019-08-15 17:11:03 +0900

    feat(comment): 코멘트 관련 코드 추가
    의견 수렴을 위해서 미완성으로 일단 커밋.

---
+++ src/main/java/com/woowacourse/zzinbros/comment/domain/Comment.java
@@ -0,0 +1,113 @@
+package com.woowacourse.zzinbros.comment.domain;
+
+import com.woowacourse.zzinbros.comment.exception.UnauthorizedException;
+import com.woowacourse.zzinbros.post.domain.Post;
+import com.woowacourse.zzinbros.user.domain.User;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @Column(nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User author;
+
+    @ManyToOne
+    @Column(nullable = false)
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    private Post post;
+
+    @Lob
+    @Column(nullable = false)
+    private String contents;
+
+    @CreationTimestamp
+    private LocalDateTime createdDateTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedDateTime;
+
+    public Comment() {
+    }
+
+    public Comment(final User author, final Post post, final String contents) {
+        this.author = author;
+        this.post = post;
+        this.contents = contents;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public LocalDateTime getUpdatedDateTime() {
+        return updatedDateTime;
+    }
+
+    public Comment update(final String contents, final User user) {
+        if (isMatchUser(user)) {
+            this.contents = contents;
+            return this;
+        }
+        throw new UnauthorizedException();
+    }
+
+    public boolean isMatchUser(final User user) {
+        return this.author.equals(user);
+    }
+
+    @Override
+    public boolean equals(final Object another) {
+        if (this == another) return true;
+        if (another == null || getClass() != another.getClass()) return false;
+        final Comment comment = (Comment) another;
+        return id.equals(comment.id) &&
+                author.equals(comment.author) &&
+                post.equals(comment.post) &&
+                Objects.equals(contents, comment.contents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, post, contents);
+    }
+
+    @Override
+    public String toString() {
+        return "Comment {" +
+                "id: " + id +
+                ", author: " + author +
+                ", post: " + post +
+                ", contents: \"" + contents + "\"" +
+                ", createdDateTime: " + createdDateTime +
+                ", updatedDateTime: " + updatedDateTime +
+                "}";
+    }
+}
