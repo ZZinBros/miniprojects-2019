247de4aab7e00343b801e0abf523c2a72158829b
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-08-20 11:13:05 +0900

    feat(controller) : UserPageController 백엔드 구현

--- src/main/java/com/woowacourse/zzinbros/user/web/controller/UserPageController.java
+++ src/main/java/com/woowacourse/zzinbros/user/web/controller/UserPageController.java
@@ -3,6 +3,7 @@ package com.woowacourse.zzinbros.user.web.controller;
 import com.woowacourse.zzinbros.post.domain.Post;
 import com.woowacourse.zzinbros.post.service.PostService;
 import com.woowacourse.zzinbros.user.domain.User;
+import com.woowacourse.zzinbros.user.exception.UserNotFoundException;
 import com.woowacourse.zzinbros.user.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,17 +14,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/mypage")
+@RequestMapping("/user-page")
 public class UserPageController {
-
     private UserService userService;
     private PostService postService;
 
+    public UserPageController(UserService userService, PostService postService) {
+        this.userService = userService;
+        this.postService = postService;
+    }
+
     @GetMapping("/{id}")
     public String showPage(@PathVariable("id") final Long id, Model model) {
-        User user = userService.findUserById(id);
-        List<Post> posts = postService.readAllByUser(user);
-        model.addAttribute("posts", posts);
-        return "/user-page";
+        try {
+            User author = userService.findUserById(id);
+            List<Post> posts = postService.readAllByUser(author);
+            model.addAttribute("posts", posts);
+            return "/user-page";
+        } catch (UserNotFoundException e) {
+            return "redirect:/";
+        }
     }
 }
