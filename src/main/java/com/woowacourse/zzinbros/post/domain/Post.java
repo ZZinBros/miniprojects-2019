bd2027cab7a955db65bb3e5392a814e8fed36ad4
kunggom <32009637+kunggom@users.noreply.github.com>
2019-08-25 15:06:49 +0900

    fix(Post): 매뉴얼에 따라 클래스에 `@DynamicInsert` 애노테이션 추가
    참고:
     * https://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#schema-generation-column-default-value

--- src/main/java/com/woowacourse/zzinbros/post/domain/Post.java
+++ src/main/java/com/woowacourse/zzinbros/post/domain/Post.java
@@ -5,6 +5,7 @@ import com.woowacourse.zzinbros.mediafile.MediaFile;
 import com.woowacourse.zzinbros.post.exception.UnAuthorizedException;
 import com.woowacourse.zzinbros.user.domain.User;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 @Entity
+@DynamicInsert
 public class Post extends BaseEntity {
     @Lob
     private String contents;
