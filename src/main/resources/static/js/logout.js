9f999b6ac9414f41c13765cf99227668989fca2c
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-08-27 10:50:18 +0900

    Update src/main/resources/static/js/logout.js
    Co-Authored-By: JYK <40727649+andole87@users.noreply.github.com>

--- src/main/resources/static/js/logout.js
+++ src/main/resources/static/js/logout.js
@@ -2,7 +2,6 @@
     const handleLogout = (event) => {
         Api.post("/logout", {})
             .then(res => {
-                console.log(res);
                 if (res.redirected) {
                     window.location.href = res.url;
                 }
