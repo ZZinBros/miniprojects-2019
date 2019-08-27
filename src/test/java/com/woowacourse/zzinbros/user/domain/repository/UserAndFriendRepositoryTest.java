729e03469545f848f27c5ffce61837fb0614a56d
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-08-27 14:44:54 +0900

    refactor(front) : 로그아웃 상태이거나 자신의 유저 페이지일 때 친구 추가 버튼 표시 X

--- src/test/java/com/woowacourse/zzinbros/user/domain/repository/UserAndFriendRepositoryTest.java
+++ src/test/java/com/woowacourse/zzinbros/user/domain/repository/UserAndFriendRepositoryTest.java
@@ -30,7 +30,7 @@ class UserAndFriendRepositoryTest extends UserBaseTest {
 
     @Test
     @DisplayName("친구 요청 추가 후 Friend 객체에서 확인")
-    void friendAddTest() {
+    void friendAddAndCheckFriend() {
         User me = userRepository.save(userSampleOf(SAMPLE_ONE));
         User first = userRepository.save(userSampleOf(SAMPLE_TWO));
         User second = userRepository.save(userSampleOf(SAMPLE_THREE));
@@ -52,7 +52,7 @@ class UserAndFriendRepositoryTest extends UserBaseTest {
 
     @Test
     @DisplayName("친구 추가 후 User 객체에서 확인")
-    void friendAddTest2() {
+    void friendAddAndCheckUser() {
         User me = userRepository.save(userSampleOf(SAMPLE_ONE));
         User first = userRepository.save(userSampleOf(SAMPLE_TWO));
         User second = userRepository.save(userSampleOf(SAMPLE_THREE));
@@ -85,7 +85,6 @@ class UserAndFriendRepositoryTest extends UserBaseTest {
                 .isInstanceOf(DataIntegrityViolationException.class);
     }
 
-
     @Test
     @DisplayName("중복된 Friend를 저장했을 때 True를 반환하는지 확인")
     void existsByFromAndToWhenAlreadyExists() {
@@ -98,8 +97,8 @@ class UserAndFriendRepositoryTest extends UserBaseTest {
     }
 
     @Test
-    @DisplayName("친구 관계인 회원을 탈퇴했을 때")
-    void deleteTest() {
+    @DisplayName("친구 관계인 회원이 탈퇴했을 때 나머지 회원은 유지된다")
+    void deleteUser() {
         User me = userRepository.save(SAMPLE_USERS.get(SAMPLE_ONE));
         User first = userRepository.save(SAMPLE_USERS.get(SAMPLE_TWO));
         friendRepository.save(Friend.of(me, first));
@@ -110,5 +109,23 @@ class UserAndFriendRepositoryTest extends UserBaseTest {
         assertEquals(first, userRepository.findById(first.getId()).orElseThrow(IllegalArgumentException::new));
         assertThatThrownBy(() -> userRepository.findById(me.getId()).orElseThrow(IllegalArgumentException::new))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("친구 관계인 회원이 탈퇴했을 때 나머지 회원은 유지된다")
+    void deleteFriend() {
+        User me = userRepository.save(SAMPLE_USERS.get(SAMPLE_ONE));
+        User first = userRepository.save(SAMPLE_USERS.get(SAMPLE_TWO));
+        Friend deletedFriend = friendRepository.save(Friend.of(me, first));
+        Friend savedFriend = friendRepository.save(Friend.of(first, me));
+
+        friendRepository.deleteById(deletedFriend.getId());
+
+        assertEquals(savedFriend, friendRepository.findById(savedFriend.getId())
+                .orElseThrow(IllegalArgumentException::new));
+        assertEquals(first, userRepository.findById(first.getId())
+                .orElseThrow(IllegalArgumentException::new));
+        assertEquals(me, userRepository.findById(me.getId())
+                .orElseThrow(IllegalArgumentException::new));
     }
 }
