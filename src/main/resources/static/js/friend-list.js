478a6173be9e2e648cbb8249e8db6ab4340ae021
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-08-29 17:41:18 +0900

    Update src/main/resources/static/js/friend-list.js
    Co-Authored-By: JYK <40727649+andole87@users.noreply.github.com>

--- src/main/resources/static/js/friend-list.js
+++ src/main/resources/static/js/friend-list.js
@@ -77,7 +77,7 @@
             });
     };
 
-    if (requestScrollBar !== null) {
+    if (requestScrollBar) {
         addElementsToScrollBar(requestScrollBar, '/friends-requests', addFriendRequest, notifyFriendRequest);
         addElementsToScrollBar(friendsScrollBar, '/friends', addFriend, () => {
         });
