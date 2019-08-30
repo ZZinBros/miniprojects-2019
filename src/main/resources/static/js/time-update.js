89f2db7df5be64b1d126d9a1018237432aea2ff2
Jo Jinsik <kunggom@gmail.com>
2019-08-31 01:01:57 +0900

    refactor(Time): 클라이언트에서 서버 시간을 받아오도록 설정
    클라이언트에서 최초 접속시에 서버의 시간을 1번 받아옵니다. 이후 새로고침이 없다면 서버로부터 시간을 받아오지 않습니다. 이렇게 하여 클라이언트의 시간 정보가 이상해도 문제가 없도록 하였습니다. 만약 서버의 시간 API를 사용할 수 없는 경우에는 클라이언트의 시간 정보를 그대로 사용합니다.

--- src/main/resources/static/js/time-update.js
+++ src/main/resources/static/js/time-update.js
@@ -1,15 +1,36 @@
-const TIMESTAMP_UPDATE_INTERVAL = 30 * 1000; // 30초마다 글자 업데이트
+const TIMESTAMP_UPDATE_INTERVAL = moment.duration({seconds: 30}); // 30초마다 글자 업데이트
 
 // Moment 라이브러리의 전역 로케일 설정
 // TODO: document.documentElement.lang 사용 - 모든 페이지에 언어 설정 필요
 moment.locale("ko");
 
+let serverTime;
+
+const getServerTime = () => {
+    fetch("/datetime")
+        .then(response => response.json())
+        .then(datetime => { serverTime = moment(datetime.datetime); })
+        .catch(ignored => { serverTime = moment(); }); // fallback
+};
+
+const addIntervalToServerTime = () => {
+    if (!serverTime) getServerTime();
+    else serverTime = moment(serverTime).add(TIMESTAMP_UPDATE_INTERVAL);
+};
+
 const updateTimeStrings = () => {
     const timeStamps = document.getElementsByTagName("time");
     for (let timeStamp of timeStamps) {
-        timeStamp.textContent = moment(timeStamp.dateTime).fromNow();
+        timeStamp.textContent = moment(timeStamp.dateTime).from(serverTime);
     }
 };
 
+const updateTimeStringsInterval = () => {
+    addIntervalToServerTime();
+    updateTimeStrings();
+};
+
+// 페이지 처음 로딩시 실행
+getServerTime();
 updateTimeStrings();
-setInterval(updateTimeStrings, TIMESTAMP_UPDATE_INTERVAL);
+setInterval(updateTimeStringsInterval, TIMESTAMP_UPDATE_INTERVAL.asMilliseconds());
