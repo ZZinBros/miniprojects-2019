982e6e0254b8a0b49116fca6b521d53a2d118395
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-08-30 14:22:09 +0900

    feat(user) : 유저 수정 페이지에서 AJAX 방식으로 이름, 이메일 수정

--- src/main/resources/static/js/user-edit.js
+++ src/main/resources/static/js/user-edit.js
@@ -1,17 +1,37 @@
-(function() {
-    const deleteBtn = document.getElementById("user-delete-btn");
+(function () {
+    const editBtn = document.getElementById("user-edit-btn");
+    const nameInput = document.getElementById('user-edit-name');
+    const emailInput = document.getElementById('user-edit-email');
+    const loginNameSpan = document.querySelector('.user-info span');
 
-    function deleteBtnHandler(event) {
+    const updateBtnHandler = (event) => {
         event.preventDefault();
 
-        const id = window.location.href.split("/")[4];
-        Api.delete(`${window.location.origin}/users/${id}`)
+        const userId = window.location.href.split("/")[4];
+        const name = nameInput.value;
+        const email = emailInput.value;
+        Api.put(`/users/${userId}`, {name, email})
             .then(res => {
-                if (res.redirected) {
-                    window.location.href = res.url;
+                if (res.status === 200) {
+                    res.json()
+                        .then(json => {
+                            const object = json.object;
+                            nameInput.value = object.name;
+                            emailInput.value = object.email;
+                            loginNameSpan.innerText = object.name;
+                            window.alert(json.message);
+                        });
                 }
-            })
-    }
+                if (res.status === 202) {
+                    res.json()
+                        .then(json => {
+                            nameInput.value = name;
+                            emailInput.value = email;
+                            window.alert(json.message);
+                        });
+                }
+            });
+    };
 
-    deleteBtn.addEventListener("click", deleteBtnHandler)
+    editBtn.addEventListener("click", updateBtnHandler);
 })();
